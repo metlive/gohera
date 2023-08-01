844c5a4dee95d935794a085bb5bdb75c1313813d
yangyunjie <yangyunjie@tal.com>
2023-08-01 22:54:30 +0800

    init add logger

--- gohera.go
+++ gohera.go
@@ -1,8 +1,37 @@
 package gohera
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
 var (
 	httpHost string
 	httpPort int
 )
 
-//错误返回为了兼容老代码，新项目不用接收返回
+func StartHttpServer() error {
+
+	httpHost = GetString("http.host")
+	httpPort = GetInt("http.port")
+	if httpPort == 0 {
+		handleError(errors.New("http host or port is not valid"))
+	}
+
+	fmt.Println("start on:" + "https://" + httpHost + ":" + strconv.Itoa(httpPort))
+
+	handleError(Engine.Run(httpHost + ":" + strconv.Itoa(httpPort)))
+	return nil
+}
+
+func handleError(err error) {
+	if err == nil || errors.Is(err, http.ErrServerClosed) {
+		return
+	}
+
+	Error(context.Background(), err, nil)
+	panic(err)
+}
