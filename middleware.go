2fda300c60afc4f3c7dd912af38316ae28407bdf
yangyunjie <yangyunjie@tal.com>
2023-08-29 19:15:48 +0800

    fix 添加cors

--- middleware.go
+++ middleware.go
@@ -1,8 +1,10 @@
 package gohera
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"net/http"
 	"strings"
 )
 
@@ -46,10 +48,39 @@ func TraceContext() gin.HandlerFunc {
 	}
 }
 
-func RequestContext() gin.HandlerFunc {
-	return func(context *gin.Context) {
+func CorsContext() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		origin := c.Request.Header.Get("Origin")
+		var headerKeys []string
+		for k := range c.Request.Header {
+			headerKeys = append(headerKeys, k)
+		}
+		//跨域自定义header ，不设置自动加入，在下面手动设置
+		cosHeader := c.GetHeader("Access-Control-Request-Headers")
+		cosHeaders := strings.Split(cosHeader, ",")
+		headerKeys = append(headerKeys, cosHeaders...)
 
+		headerStr := strings.Join(headerKeys, ", ")
+		if headerStr != "" {
+			headerStr = fmt.Sprintf("access-control-allow-origin, access-control-allow-headers, %s", headerStr)
+		} else {
+			headerStr = "access-control-allow-origin, access-control-allow-headers"
+		}
+		if origin != "" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Access-Control-Allow-Headers", headerStr)
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+			c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
+
+		//放行所有OPTIONS方法
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatusJSON(http.StatusNoContent, "WeclassRoom Request Options")
+		}
+		c.Next()
 	}
+
 }
 
 func RecoveryContext() gin.HandlerFunc {
